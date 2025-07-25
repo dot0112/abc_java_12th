eee732b9bf208a648981026cc759d2e71e906d25
dot0112 <dot0112dot0112@gmail.com>
2025-07-25 14:01:03 +0900

    25-07-25 14:01

---
+++ src/ps/bronze/p1000.java
@@ -0,0 +1,13 @@
+package ps.bronze;
+
+import java.util.Scanner;
+
+public class p1000 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println(s.nextInt() + s.nextInt());
+
+        s.close();
+    }
+}
