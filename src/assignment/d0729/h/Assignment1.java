acf0be20759ef54fab892fb9e07ae6ffe0179026
dot0112 <dot0112dot0112@gmail.com>
2025-07-31 09:59:23 +0900

    25-07-31:1

--- src/assignment/d0729/h/Assignment1.java
+++ src/assignment/d0729/h/Assignment1.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Assignment1 {
     static Scanner s = new Scanner(System.in);
 
+    /**
+     * 태어난 해를 입력받고 윤년 계산, 출력을 하는 메서드
+     */
     static void leapYear() {
         System.out.print("\n태어난 해가 윤년인지 확인하여 결과를 출력하세요\n태어난 해를 입력해주세요: ");
         int year = s.nextInt();
@@ -13,6 +16,9 @@ public class Assignment1 {
         else System.out.println("평년입니다");
     }
 
+    /**
+     * 키와 몸무게를 입력받고 BMI 지수를 계산, 출력하는 메서드
+     */
     static void bmiCalculator() {
         int kg;
         double bmi, m;
@@ -45,6 +51,9 @@ public class Assignment1 {
         System.out.printf("당신의 BMI는 %7.2f이며, '%s'입니다.\n", bmi, bmiStatus);
     }
 
+    /**
+     * 각 항과 사칙연산 부호를 입력받고 결과를 출력하는 메서드
+     */
     static void calculator() {
         int lhs, rhs;
         char op;
@@ -88,6 +97,9 @@ public class Assignment1 {
         System.out.printf(oFormat + '\n', result);
     }
 
+    /**
+     * 구구단 표를 출력하는 메서드
+     */
     static void multiplication() {
         int dan, times;
         for (dan = 2; dan <= 9; dan++) {    // 1단부터 9단까지 반복하는 외부 반복문
@@ -97,6 +109,9 @@ public class Assignment1 {
         }
     }
 
+    /**
+     * 정수 값을 입력받고 합계를 출력하는 메서드
+     */
     static void accumulate() {
         int sum, i, n = -1;
 
@@ -110,18 +125,97 @@ public class Assignment1 {
 
     }
 
+    /**
+     * 생년을 입력받고 띠를 출력하는 메서드
+     */
+    static void chineseZodiac() {
+        String[] chineseZodiac =
+                {"쥐", "소", "범", "토끼",
+                        "용", "뱀", "말", "양",
+                        "원숭이", "닭", "개", "돼지",};
+
+        System.out.print("\n태어난 년도를 입력하세요: ");
+        int year = s.nextInt();
+        System.out.printf("띠: %s띠\n", chineseZodiac[(year + 8) % 12]);
+    }
+
+    /**
+     * 성적을 입력받고 평균과 등급을 출력하는 메서드
+     */
+    public static void gradeCal() {
+        int[] scores = new int[3];
+        int sum = 0;
+        double avg;
+
+        System.out.print("\n성적을 입력하세요 (국어, 영어, 수학): ");
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = s.nextInt();
+            sum += scores[i];
+        }
+
+        avg = sum / (double) scores.length;
+
+        System.out.println();
+
+        System.out.print("과목:\t");
+        for (String subject : new String[]{"국어", "영어", "수학"}) {
+            System.out.print(subject + "\t");
+        }
+        System.out.println();
+
+        System.out.print("점수:\t");
+        for (int score : scores) {
+            System.out.print(score + "\t");
+        }
+
+        System.out.print("\n\n");
+
+        System.out.printf("총점:\t%d\n", sum);
+
+        System.out.printf("평균:\t%.2f\n", avg);
+
+        System.out.printf("등급:\t%c\n", whatGrade((int) avg));
+
+        System.out.println();
+    }
+
+    /**
+     * 성적에 대해 등급을 반환하는 메서드
+     *
+     * @param score 점수
+     * @return char 형의 등급 값
+     */
+    public static char whatGrade(int score) {
+        return switch (score / 10) {
+            case 10, 9 -> 'A';
+            case 8 -> 'B';
+            case 7 -> 'C';
+            case 6 -> 'D';
+            default -> 'F';
+        };
+    }
+
+    /**
+     * 사용자가 선택 가능한 목록을 출력하는 메서드
+     */
+    static void printMenu() {
+        System.out.print("""
+                
+                [선택 항목]
+                1: 윤년계산기
+                2: bmi계산기
+                3: 사칙연산 계산기
+                4: 구구단
+                5: 누산합 계산기
+                6: 띠 계산기
+                7: 성적 계산기
+                0: 종료
+                번호 입력:\s""");
+    }
+
     public static void main(String[] args) {
         while (true) {
-            System.out.print("""
-                    
-                    [선택 항목]
-                    1: 윤년계산기
-                    2: bmi계산기
-                    3: 사칙연산 계산기
-                    4: 구구단
-                    5: 누산합 계산기 
-                    0: 종료
-                    번호 입력:\s""");
+            printMenu();
             int n = s.nextInt();
             if (n == 0) break;
             switch (n) {
@@ -130,6 +224,8 @@ public class Assignment1 {
                 case 3 -> calculator();
                 case 4 -> multiplication();
                 case 5 -> accumulate();
+                case 6 -> chineseZodiac();
+                case 7 -> gradeCal();
             }
         }
         s.close();
